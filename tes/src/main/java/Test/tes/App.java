4949de091547106ed684e231b79336f229b68a5f
Administrator <Administrator@bhavani-3>
2019-07-31 06:29:57 -0700

    initial commit

---
+++ tes/src/main/java/Test/tes/App.java
@@ -0,0 +1,16 @@
+package Test.tes;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public void hi()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tes/src/main/java/Test/tes/App1.java
@@ -0,0 +1,12 @@
+package Test.tes;
+
+import org.testng.annotations.Test;
+
+public class App1 {
+
+	
+	@Test
+	void abc() {
+		System.out.println("abc test method");
+	}
+}
